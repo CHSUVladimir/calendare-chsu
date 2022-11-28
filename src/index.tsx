409f8d3611c016d72c8723d0063b4d3ac7c711ec
Владимир Трунов <vetrunov@chsu.ru>
2022-11-28 10:26:32 +0300

    rebuild with examples and unset autobuild

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import  './App.css';
-import App from './Calendare';
+import App, { ICHSUCalendare } from './Calendare';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
 
 export {default as CHSUCalendare} from './Calendare';
-
+Object.defineProperty(global,'buildCalendare',{
+  value:(placeId:string,sett?:ICHSUCalendare[]|ICHSUCalendare)=>{
+    const root = ReactDOM.createRoot(
+      document.getElementById(placeId) as HTMLElement
+    );
+    if(sett){
+      const arr= sett as ICHSUCalendare[];
+      if(arr.length){
+        root.render(
+          <React.StrictMode>
+            {arr.map((e, i)=>{
+              return (
+                <App {...e} key={(e.StartDay??"FirstDayMonth")+"_"+i}/>
+              );
+            })}
+          </React.StrictMode>
+        );
+      }else{
+        const cs= sett as ICHSUCalendare;
+        root.render(
+          <React.StrictMode>
+            <App {...cs}/>
+          </React.StrictMode>
+        );
+      }
+    }else{
+      root.render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+    }
+    
+  }
+})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
